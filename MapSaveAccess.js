c5e135fbb810e11c24f1962e95fe0002dac388ae
Karberus <karberus@outlook.com>
2015-12-07 22:16:17 -0500

    Update MapSaveAccess.js

--- MapSaveAccess.js
+++ MapSaveAccess.js
@@ -1,7 +1,7 @@
 //============================================================================
 // Karberus - Map Save Access
 // MapSaveAccess.js
-// Version 1.01
+// Version 1.02
 // No credit required. Can be used commercially or non commercially
 //============================================================================
 //============================================================================
@@ -13,9 +13,9 @@ Karberus.MapSaveAcc = Karberus.MapSaveAcc || {};
 //============================================================================
 //============================================================================
 /*:
- * @plugindesc v1.01 Allows you to set which maps will have Save Access disabled by default.
+ * @plugindesc v1.02 Allows you to set which maps will have Save Access disabled by default.
  * @author Karberus
- * @version 1.01
+ * @version 1.02
  *
  *
  *
@@ -28,12 +28,27 @@ Karberus.MapSaveAcc = Karberus.MapSaveAcc || {};
  *@help This Plugin allows you to disable save access by default every time
  *you enter a specified Map.
  *
- *Set up the Map ID of the maps that will have disabled save access.
+ *Set up the Map ID's of the maps that will have disabled save access.
  *
  *Whenever you enter a map that is not specified, save access will be enabled
  *once again.
  *
+ *If you wish to change a map's default save access to enabled mid game, use
+ *the plugin command MapEnableSave MapID.
  *
+ *
+ * //==============================================================================
+ * //                  Plugin Commands
+ * //==============================================================================
+ *
+ * //Changes the default save access of a map from disabled to enabled.
+ *
+ * MapEnableSave x     //where x is the MapID of the map you wish to have enabled
+ *                       save access by default.
+ *
+ *
+ * //==============================================================================
+ * //==============================================================================
  *
  */
  (function() {
@@ -46,13 +61,25 @@ Karberus.MapSaveAcc = Karberus.MapSaveAcc || {};
    	Karberus.MapSaveAcc.MapsDisabledSave[i] = parseInt(Karberus.MapSaveAcc.MapsDisabledSave[i]);
    }
 
+   Karberus.MapSaveAcc.MapsEnabledSave = [];
+
+   var Karb_Game_Interpreter_pluginCommand = Game_Interpreter.prototype.pluginCommand;
+
+Game_Interpreter.prototype.pluginCommand = function(command, args) {
+           Karb_Game_Interpreter_pluginCommand.call(this, command, args);
+            if (command === "MapEnableSave") {
+              Karberus.MapSaveAcc.MapsEnabledSave.push(Number(args));
+            }
+   };
+
 //==========================================================================================
 _Karb_Game_Map_setup = Game_Map.prototype.setup;
 Game_Map.prototype.setup = function(mapId)  {
 
   _Karb_Game_Map_setup.call(this, mapId);
-
-   if (Karberus.MapSaveAcc.MapsDisabledSave.indexOf($gameMap._mapId) > -1)
+console.log(Karberus.MapSaveAcc.MapsEnabledSave);
+   if (Karberus.MapSaveAcc.MapsDisabledSave.indexOf($gameMap._mapId) > -1 &&
+ Karberus.MapSaveAcc.MapsEnabledSave.indexOf($gameMap._mapId) < 0)
    {
      $gameSystem.disableSave();
    }
